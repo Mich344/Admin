cb95044805042670ac56115d1806358f25766647
Mich344 <104693039+Mich344@users.noreply.github.com>
2022-08-15 17:39:36 -0500

    Add files via upload

---
+++ Productos.php
@@ -0,0 +1,70 @@
+<?php
+include_once "Basedata.php";
+// Llamar la base de datos desde el include_once.
+$con = mysqli_connect($host, $user, $pasword, $db);
+?>
+
+
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1><strong>Productos</strong></h1>
+        </div>
+
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+
+            <!-- /.card-header -->
+            <div class="card-body">
+              <table Id="Tablaproductos" class="table table-bordered table-hover">
+                <thead>
+                  <tr>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Cantidad</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <?php
+                  $query = "SELECT Id, Nombre, Cantidad, Precio  from productos; ";
+                  //Restultados 
+                  $res = mysqli_query($con, $query);
+                  while ($row = mysqli_fetch_assoc($res)) {
+                  ?>
+                    <tr>
+                      <td><?php echo $row['Nombre'] ?></td>
+                      <td><?php echo $row['Precio'] ?></td>
+                      <td><?php echo $row['Cantidad'] ?></td>
+                    </tr>
+                  <?php
+                  }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
