fc55cab05f406d40a87a6f5d351965d421f238ac
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-08-15 18:30:25 -0500

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -6,15 +6,21 @@ ini_set('display_errors', '1');
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Database user / pass
- */
+ * Database user / pass */
+ <?php
+   	include_once "Basedata.php";
+   	// Llamar la base de datos desde el include_once.
+   	$con = mysqli_connect($host, $user, $pasword, $db);
+	$res = mysqli_query($con, $query);
+ ?>
+
 $sql_details = array(
 	"type" => "Mysql",     // Database type: "Mysql", "Postgres", "Sqlserver", "Sqlite" or "Oracle"
-	"user" => "",          // Database user name
-	"pass" => "",          // Database password
-	"host" => "", // Database host
+	"user" => "$user",          // Database user name
+	"pass" => "$pasword",          // Database password
+	"host" => "$host", // Database host
 	"port" => "",          // Database connection port (can be left empty for default)
-	"db"   => "",          // Database name
+	"db"   => "$db",          // Database name
 	"dsn"  => "",          // PHP DSN extra information. Set as `charset=utf8mb4` if you are using MySQL
 	"pdoAttr" => array()   // PHP PDO attributes array. See the PHP documentation for all options
 );
