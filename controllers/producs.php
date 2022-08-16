16e6d9649257f658e14a079b0328472fa9cff94b
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-08-15 19:25:20 -0500

    Update producs.php

--- controllers/producs.php
+++ controllers/producs.php
@@ -21,5 +21,21 @@ use
 
 // Build our Editor instance and process the data coming from _POST
 include_once("../Basedata.php");
+	Editor::inst( $db, 'productos' )
+		->fields(
+			Field::inst( 'Nombre' )
+			// validar 
+				->validator( Validate::notEmpty( ValidateOptions::inst()
+					->message( 'Por favor ingresa el nombre del producto' )	
+				) ),
+			Field::inst( 'Precio' )
+				->validator( Validate::numeric() )
+				->setFormatter( Format::ifEmpty(null) ),
+			Field::inst( 'Cantidad' )
+				->validator( Validate::numeric() )
+				->setFormatter( Format::ifEmpty(null) )
+		)
+		->process( $_POST )
+		->json();
 
 ?>
