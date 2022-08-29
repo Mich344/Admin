4d7c7322d252ac294e5564f2a77b6a3745f26108
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-08-29 17:40:24 -0500

    Create CrearP.php

---
+++ CrearP.php
@@ -0,0 +1,93 @@
+<?php 
+ if (isset($_REQUEST['guardar'])){
+  include_once "Basedata.php";
+  // Llamar la base de datos desde el include_once.
+  $con = mysqli_connect($host, $user, $pasword, $db);
+  
+  $Nombre = mysqli_real_escape_string($con, $_REQUEST['nombre']?? '');
+  $Precio = mysqli_real_escape_string($con, $_REQUEST['precio']?? '');
+  $Cantidad = mysqli_real_escape_string($con, $_REQUEST['cantidad']?? '');
+  $talla = mysqli_real_escape_string($con, $REQUEST['talla']?? '');
+  $descripcion = mysqli_real_escape_string($con, $REQUEST['descripcion']?? '');
+  
+  $query = "INSERT INTO productos (nombre,precio,cantidad,talla,descripcion) VALUES ('".$nombre."' , '".$precio."' , '".$cantidad."' , '".$talla."'
+  , '".$descripcion."'); ";
+  //Restultados 
+  
+  $res = mysqli_query($con, $query);
+  if($res){
+    echo '<meta http-equiv= "refresh" content="0; url=Panel.php?modulo=Productos&mensaje=Producto creado correctamente" />';
+  }
+  else {
+?>
+ <div class="alert alert-danger" role="alert">
+   Error al agregar producto <?php echo mysqli_error($con)?>
+ </div>
+<?php
+  }
+ }
+
+
+
+?> 
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1><strong>Agregar Productos</strong></h1>
+        </div>
+
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+
+            <!-- /.card-header -->
+            <div class="card-body">
+              <form action="Panel.php?modulo=CrearP" method="post">
+                <div class="for-group">
+                  <label>Nombre</label>
+                  <input type="text" name="nombre"  class="form-control" required = "" >
+                </div>
+                  <div class="for-group">
+                    <label>Precio</label>
+                    <input type="number" name="precio" class="form-control" required = "" >
+                  </div>
+                  <div class="for-group">
+                  <label>Cantidad</label>
+                  <input type="number" name="cantidad"  class="form-control" required = "" >
+                </div>
+               <div class="for-group">
+                  <label>Talla</label>
+                  <input type="text" name="talla"  class="form-control" required = "" >
+                </div>
+               <div class="for-group">
+                  <label>Descripción</label>
+                  <input type="text" name="descripcion"  class="form-control" required = "" >
+                </div>
+                <div class="for-group">
+                    <br>
+                  <button type="submit" class="btn btn-primary" name="guardar">guardar</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.card -->
+
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
