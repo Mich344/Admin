17f8d499bac7b72ee9fab6fcb0c579a5efa6a611
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-08-29 14:39:48 -0500

    Update Estadisticas.php

--- Estadisticas.php
+++ Estadisticas.php
@@ -44,7 +44,7 @@
                 <div class="inner">
                   <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-                  <p>Bounce Rate</p>
+                  <p>Porcentaje de ventas</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-stats-bars"></i>
