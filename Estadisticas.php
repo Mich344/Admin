af836797646a7eb2740f7689fbd0c7034076cdf8
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-08-31 20:38:59 -0500

    Update Estadisticas.php

--- Estadisticas.php
+++ Estadisticas.php
@@ -99,10 +99,10 @@
                   <div class="card-tools">
                     <ul class="nav nav-pills ml-auto">
                       <li class="nav-item">
-                        <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
+                        <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Lineal</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
+                        <a class="nav-link" href="#sales-chart" data-toggle="tab">Circular</a>
                       </li>
                     </ul>
                   </div>
