fbc07ec3794c94368831e2e8a38f44efe140ad11
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-08-31 19:02:35 -0500

    Update Estadisticas.php

--- Estadisticas.php
+++ Estadisticas.php
@@ -236,9 +236,6 @@
                     <button type="button" class="btn btn-success btn-sm" data-card-widget="collapse">
                       <i class="fas fa-minus"></i>
                     </button>
-                    <button type="button" class="btn btn-success btn-sm" data-card-widget="remove">
-                      <i class="fas fa-times"></i>
-                    </button>
                   </div>
                   <!-- /. tools -->
                 </div>
