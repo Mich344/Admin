643a4de16521e75ee0547f0021ef220c922e7360
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-09-05 11:36:43 -0500

    Update single.php

--- single.php
+++ single.php
@@ -275,26 +275,31 @@ $(window).load(function() {
 	<!--//header-->
 	<!--banner-->
 	<div class="single">
+	<?php 
+		/*INCLUIMOS LA BASE DE DATOS AL INDEX*/
+		include_once "Basedata.php";
+		/*CONECTA LA BASE DE DATOS (BASEDATA) CON EL LOCALHOST, USUARIO, CONTRASEÑA Y DB*/
+		$con=mysqli_connect($host, $user, $pasword, $db);
+		/* UTILIZA UN REQUERIMIENTO DONDE SE VA A SELECCIONAR TODOS LOS PRODUCTOS CON SU ID, NOMBRE, PRECIO Y CANTIDAD DE LA TABLA PRODUCTOS  */
+		$Id= mysqli_real_escape_string($con,$_REQUEST['Id']??'');
+		$queryproducto="SELECT Id,nombre,precio,cantidad, talla, descripcion, imagen FROM productos WHERE Id='$Id';";
+		/* DA UNA RESPUESTA CON EL MYSQL CON LOS REQUERIMIENTOS Y LA CONEXION */
+		$resproducto=mysqli_query($con, $queryproducto);
+		$rowproducto=mysqli_fetch_assoc($resproducto);
+	?>
 		<div class="container">
 			<div class="single-info">		
 				<div class="col-md-6 single-top wow fadeInLeft animated" data-wow-delay=".5s">	
 					<div class="flexslider">
 						<ul class="slides">
 							<li data-thumb="images/s1.jpg">
-								<div class="thumb-image"> <img src="images/s1.jpg" data-imagezoom="true" class="img-responsive" alt=""> </div>
-							</li>
-							<li data-thumb="images/s2.jpg">
-								 <div class="thumb-image"> <img src="images/s2.jpg" data-imagezoom="true" class="img-responsive" alt=""> </div>
-							</li>
-							<li data-thumb="images/s3.jpg">
-							   <div class="thumb-image"> <img src="images/s3.jpg" data-imagezoom="true" class="img-responsive" alt=""> </div>
+								<div class="thumb-image"><?php echo $rowproducto['Id'] ?>"><?php echo "<img src='upload/".$rowproducto['imagen']."' data-imagezoom='true' class='img-responsive' alt=''>"?> </div>
 							</li> 
 						</ul>
 					</div>
 				</div>
-			</div>
 				<div class="col-md-6 single-top-left simpleCart_shelfItem wow fadeInRight animated" data-wow-delay=".5s">
-					<h3>Buso capucha estilo deportivo</h3>
+					<h3><?php echo $rowproducto['nombre'] ?></h3>
 					<div class="single-rating">
 						<span class="starRating">
 							<input id="rating5" type="radio" name="rating" value="5" checked>
@@ -308,24 +313,18 @@ $(window).load(function() {
 							<input id="rating1" type="radio" name="rating" value="1">
 							<label for="rating1">1</label>
 						</span>
-						<p>Calificacion</p>
-						<a href="#">Tu reseña</a>
+						<p>5.00 out of 5</p>
+						<a href="#">Add Your Review</a>
 					</div>
-					<h6 class="item_price">$600.00</h6>			
-					<p>En este preciso momento, personas de todo el mundo están pasando su día y haciendo lo que tienen que hacer luciendo el logo adidas Badge of Sport. Es un símbolo unificador del compromiso, la pasión y el deporte, y este buzo con capucha adidas lo luce con orgullo en el pecho. Ponte el tuyo y únete a la comunidad global.</p>
+					<h6 class="item_price"><?php echo $rowproducto['precio'] ?></h6>			
+					<p><?php  echo $rowproducto['descripcion'] ?></p>
 					
 					<ul class="size">
-						<li>Talla</li>
-						<li><a href="#">XS</a></li>
-						<li><a href="#">S</a></li>
-						<li><a href="#">M</a></li>
-						<li><a href="#">L</a></li>
-						<li><a href="#">XL</a></li>
-						<li><a href="#">XXL</a></li>
+						<h4>Size</h4>
+						<li><a href="#">6-12 M</a></li>
 					</ul>
-					
 					<ul class="color">
-						 <li> Color</li>
+						<h4>Color</h4>
 						<li><a href="#"></a></li>
 						<li><a href="#" class="red"></a></li>
 						<li><a href="#" class="green"></a></li>
@@ -333,10 +332,10 @@ $(window).load(function() {
 					</ul>
 					<div class="clearfix"> </div>
 					<div class="quantity">
-						<p class="Cantidad"> Cantidad :  </p><input min="1" type="number" value="1" class="item_quantity">
+						<p class="qty"> cantidad:  </p><input min="1" type="number" value="1" max="<?php echo $rowproducto['cantidad'] ?>" class="item_quantity">
 					</div>
 					<div class="btn_form">
-						<a href="#" class="add-cart item_add">Añadir al carrito</a>	
+						<a href="#" class="add-cart item_add">ADD TO CART</a>	
 					</div>
 				</div>
 			   <div class="clearfix"> </div>
@@ -348,13 +347,13 @@ $(window).load(function() {
 						<div class="panel-heading" role="tab" id="headingOne">
 							<h4 class="panel-title">
 								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-								  Descripcion.
+								  Description
 								</a>
 							</h4>
 						</div>
 						<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
 							<div class="panel-body">
-								Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+							<?php echo $rowproducto['descripcion'] ?>
 							</div>
 						</div>
 					</div>
@@ -362,7 +361,7 @@ $(window).load(function() {
 						<div class="panel-heading" role="tab" id="headingTwo">
 							<h4 class="panel-title">
 								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-								   Informacion adicional.
+								   additional information
 								</a>
 							</h4>
 						</div>
@@ -376,7 +375,7 @@ $(window).load(function() {
 						<div class="panel-heading" role="tab" id="headingThree">
 							<h4 class="panel-title">
 								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-									Reseñas.
+									reviews (5)
 								</a>
 							</h4>
 						</div>
@@ -390,13 +389,13 @@ $(window).load(function() {
 						<div class="panel-heading" role="tab" id="headingFour">
 							<h4 class="panel-title">
 								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-									Ayuda.
+									help
 								</a>
 							</h4>
 						</div>
 						<div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
 							<div class="panel-body">
-								Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								Para cualquier duda puedes contactarnos a nuestro correo o a nuestro número. Dirigete al siguiente link para que puedas accerder a ellos <a href="products.php"> este link 
 							</div>
 						</div>
 					</div>
@@ -404,9 +403,116 @@ $(window).load(function() {
 			</div>
 			<!--//collapse -->
 			<!--related-products-->
-			<div class="products">	 
-		<div class="container">
-			<div class="gallery-info">
+			<div class="related-products">
+				<div class="title-info wow fadeInUp animated" data-wow-delay=".5s">
+					<h3 class="title">Related<span> Products</span></h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit curabitur </p>
+				</div>
+				<div class="related-products-info">
+					<div class="col-md-3 new-grid simpleCart_shelfItem wow flipInY animated" data-wow-delay=".5s">
+						<div class="new-top">
+							<a href="single.html"><img src="images/g9.jpg" class="img-responsive" alt=""/></a>
+							<div class="new-text">
+								<ul>
+									<li><a class="item_add" href=""> Add to cart</a></li>
+									<li><a href="single.html">Quick View </a></li>
+									<li><a href="products.html">Show Details </a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="new-bottom">
+							<h5><a class="name" href="single.html">Baby Red Dress </a></h5>
+							<div class="rating">
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span>☆</span>
+							</div>	
+							<div class="ofr">
+								<p class="pric1"><del>$2000.00</del></p>
+								<p><span class="item_price">$500</span></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 new-grid new-mdl simpleCart_shelfItem wow flipInY animated" data-wow-delay=".7s">
+						<div class="new-top">
+							<a href="single.html"><img src="images/g10.jpg" class="img-responsive" alt=""/></a>
+							<div class="new-text">
+								<ul>
+									<li><a class="item_add" href=""> Add to cart</a></li>
+									<li><a href="single.html">Quick View </a></li>
+									<li><a href="products.html">Show Details </a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="new-bottom">
+							<h5><a class="name" href="single.html">Crocs Sandals </a></h5>
+							<div class="rating">
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span>☆</span>
+							</div>	
+							<div class="ofr">
+								<p class="pric1"><del>$2000.00</del></p>
+								<p><span class="item_price">$500</span></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 new-grid new-mdl1 simpleCart_shelfItem wow flipInY animated" data-wow-delay=".9s">
+						<div class="new-top">
+							<a href="single.html"><img src="images/g11.jpg" class="img-responsive" alt=""/></a>
+							<div class="new-text">
+								<ul>
+									<li><a class="item_add" href=""> Add to cart</a></li>
+									<li><a href="single.html">Quick View </a></li>
+									<li><a href="products.html">Show Details </a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="new-bottom">
+							<h5><a class="name" href="single.html">Pink Sip Cup </a></h5>
+							<div class="rating">
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span>☆</span>
+							</div>	
+							<div class="ofr">
+								<p class="pric1"><del>$2000.00</del></p>
+								<p><span class="item_price">$500</span></p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 new-grid simpleCart_shelfItem wow flipInY animated" data-wow-delay="1.1s">
+						<div class="new-top">
+							<a href="single.html"><img src="images/g12.jpg" class="img-responsive" alt=""/></a>
+							<div class="new-text">
+								<ul>
+									<li><a class="item_add" href=""> Add to cart</a></li>
+									<li><a href="single.html">Quick View </a></li>
+									<li><a href="products.html">Show Details </a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="new-bottom">
+							<h5><a class="name" href="single.html">Child Print Bike </a></h5>
+							<div class="rating">
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span class="on">☆</span>
+								<span>☆</span>
+							</div>	
+							<div class="ofr">
+								<p class="pric1"><del>$2000.00</del></p>
+								<p><span class="item_price">$500</span></p>
+							</div>
+						</div>
+					</div>
 					<div class="clearfix"> </div>
 				</div>
 			</div>
@@ -475,3 +581,4 @@ $(window).load(function() {
     <script src="js/bootstrap.js"></script>
 </body>
 </html>
+
