55cf8de138b3469803d2ba55fcdd6984c11d11fb
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-09-09 16:12:24 -0500

    Update pago.php

--- pago.php
+++ pago.php
@@ -6,15 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://www.paypal.com/sdk/js?client-id=AT6WdzGS4YhY5QWNxVPFpAOldbBnRFUsXauhJ7XgPD2Nyj3lm7HolEQGd0mbvl1sfiL0gyNhRQn308It& currency=COP"></scrip>
+    <script src="https://www.paypal.com/sdk/js?client-id=AT6WdzGS4YhY5QWNxVPFpAOldbBnRFUsXauhJ7XgPD2Nyj3lm7HolEQGd0mbvl1sfiL0gyNhRQn308It& currency=COP"></script>
 </head>
 <body>
 <div id="paypal-buttom-container"></div>    
 <script> 
 paypal.Buttons().render('#paypal-buttom-container');
 </script>
-</body>
-    
+</body>   
 </html>
-
-
