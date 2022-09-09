84d9e266e7e77fb349668194207de49ee21ab10b
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-09-08 19:44:58 -0500

    Update pago.php

--- pago.php
+++ pago.php
@@ -8,7 +8,7 @@
     <script src="https://www.paypal.com/sdk/js?client-id=AT6WdzGS4YhY5QWNxVPFpAOldbBnRFUsXauhJ7XgPD2Nyj3lm7HolEQGd0mbvl1sfiL0gyNhRQn308It currency=COP"></scrip
 </head>
 <body>
-<div id="paypal - buttom.conteiner"></div>    
+<div id="paypal-buttom-conteiner"></div>    
 </body>
 </html>
 
