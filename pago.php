5501ae04359a8c07edaa11c66072781407bf5e17
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-09-08 19:36:50 -0500

    Update pago.php

--- pago.php
+++ pago.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-     <script src="https://www.paypal.com/sdk/js?client-id=test&currency=USD"></scrip
+     <script src="https://www.paypal.com/sdk/js?client-id="CODE CLIENTE"></scrip
 </head>
 <body>
     
