3e410439a24cbd8009e64cbcfa9e0c9f6e932792
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-09-09 16:33:46 -0500

    Update pago.php

--- pago.php
+++ pago.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-    
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://www.paypal.com/sdk/js?client-id=AT6WdzGS4YhY5QWNxVPFpAOldbBnRFUsXauhJ7XgPD2Nyj3lm7HolEQGd0mbvl1sfiL0gyNhRQn308ItAT6WdzGS4YhY5QWNxVPFpAOldbBnRFUsXauhJ7XgPD2Nyj3lm7HolEQGd0mbvl1sfiL0gyNhRQn308It& 
-    currency=COP"></script>
-</head>
-<body>
 <div id="smart-button-container">
       <div style="text-align: center;">
         <div id="paypal-button-container"></div>
@@ -56,5 +44,3 @@
     }
     initPayPalButton();
   </script>
-</body>   
-</html>
