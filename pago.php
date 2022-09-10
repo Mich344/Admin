b1d876243855427d9c578ceef730404414a36fae
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-09-10 14:36:37 -0500

    Update pago.php

--- pago.php
+++ pago.php
@@ -1,14 +1,101 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang = "es">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!--   <link rel="stylesheet" href="css/pagos.css" type="text/css" media="screen" />
-  <script src="js/pagos.js"></script> -->
-  <title>Pagos</title>
+<title>SportsWearLine</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Modern Shoppe Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--//for-mobile-apps -->
+<!--Custom Theme files -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<!--//Custom Theme files -->
+<!--js-->
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/modernizr.custom.js"></script>
+<!--//js-->
+<!--cart-->
+<script src="js/simpleCart.min.js"></script>
+<!--cart-->
+<!--Libreria iconos rrss-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<!--web-fonts-->
+<link href='//fonts.googleapis.com/css?family=Raleway:400,100,100italic,200,200italic,300,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic' rel='stylesheet' type='text/css'><link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Pompiere' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Fascinate' rel='stylesheet' type='text/css'>
+<!--web-fonts-->
+<!--animation-effect-->
+<link href="css/animate.min.css" rel="stylesheet"> 
+<script src="js/wow.min.js"></script>
+	<script>
+	 new WOW().init();
+	</script>
+<!--//animation-effect-->
+<!--start-smooth-scrolling-->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>	
+<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+</script>
+<!--//end-smooth-scrolling-->
 </head>
 <body>
+	<!--header-->
+				<div class="nav navbar-nav logo wow zoomIn animated" data-wow-delay=".7s">
+					<h1><a style="color: #B2051F">SPORTS<b>WEARLINE</b><span class="tag"><strong> Vístete bien, hoy tienes una cita con el deporte ¡Marca tú estilo!</strong></span></a></h1>
+				</div>
+	<!--//header-->
+	<!-- the jScrollPane script -->
+	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+			<script type="text/javascript" id="sourcecode">
+				$(function()
+				{
+					$('.scroll-pane').jScrollPane();
+				});
+			</script>
+	<!-- //the jScrollPane script -->
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<!-- the mousewheel plugin -->
+	<!--search jQuery-->
+	<script src="js/main.js"></script>
+		 <script type="text/javascript">
+			var sound = new Audio();
+			sound.src = "../images/Audio/Productos.mp3"
+		</script>
+	<!--//search jQuery-->
+	<!--smooth-scrolling-of-move-up-->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		
+			var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+			};
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+	<!--//smooth-scrolling-of-move-up-->
+	<!--Bootstrap core JavaScript
+    ================================================== -->
+    <!--Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap.js"></script>
+	</body>
+</html>
+
 
 <div class="card" style="width: 18rem;">
   <div class="card-body">
