152b1e778909a90b8c55cdc67d29315511f5a5f4
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-09-10 15:46:30 -0500

    Update pago.php

--- pago.php
+++ pago.php
@@ -43,25 +43,14 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		    <div class="three"></div>
 		    <div class="four"></div>
 		  </div>
-		  <p class="pick">choose size</p>
-		  <div class="sizes">
-		    <div class="size">5</div>
-		    <div class="size">6</div>
-		    <div class="size">7</div>
-		    <div class="size">8</div>
-		    <div class="size">9</div>
-		    <div class="size">10</div>
-		    <div class="size">11</div>
-		    <div class="size">12</div>
-		  </div>
 		  <div class="product">
-		    <p>Women's Running Shoe</p>
-		    <h1>Nike Epic React Flyknit</h1>
-		    <h2>$150</h2>
-		    <p class="desc">The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.</p>
+			<p>Nombre</p>
+		    	<h2>Precio</h2>
+		    <p class="desc">Descripcion</p>
 		    <div class="buttons">
-		      <button class="add">Add to Cart</button>
-		      <button class="like"><span>♥</span></button>
+			<p>Talla elegida</p>
+			<p>Color elegida</p>
+			<p>Cantidad de productos elegidos</p>
 		    </div>
 		  </div>
 	</div>
