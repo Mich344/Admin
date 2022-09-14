3e943ba74b1a4fd8e7195b342990ddada6e3f363
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-09-13 19:41:59 -0500

    Update pago.php

--- pago.php
+++ pago.php
@@ -36,6 +36,16 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque quam quos vitae. Sed libero et nulla suscipit quae nostrum distinctio corrupti cumque, cum porro accusantium, ullam sit dolores quas nisi!</p>
 		 <br>
 		</div>
+	        
+		  <div class="caja__columna">
+		<svg> <rect> </rect> </svg>
+		<div class="Texto">
+		<h2> Información </h2>
+		<p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque quam quos vitae. Sed libero et nulla suscipit quae nostrum distinctio corrupti cumque, cum porro accusantium, ullam sit dolores quas nisi!</p>
+		 <br>
+		</div>
+			
+			
 			
 			<!-- DIVISONES -->
 			
