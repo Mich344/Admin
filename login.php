3b65ba2a58842e75c12d06fe62e88c3e56e175f4
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-09-15 12:32:45 -0500

    Update login.php

--- login.php
+++ login.php
@@ -78,19 +78,7 @@
               <button type="submit" class="btn btn-primary btn-block" name="login">Ingresar</button>
             </div>
             <!-- /.col -->
-          </div>
-        </form>
 
-        <div class="social-auth-links text-center mb-3">
-          <p>- OR -</p>
-          <a href="#" class="btn btn-block btn-primary">
-            <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-          </a>
-          <a href="#" class="btn btn-block btn-danger">
-            <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-          </a>
-        </div>
-        <!-- /.social-auth-links -->
 
         <p class="mb-1">
           <a href="forgot-password.html">I forgot my password</a>
