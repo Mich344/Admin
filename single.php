f4693fd205279a606efbf50d13b38932fe2a9221
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-09-19 20:24:58 -0500

    Update single.php

--- single.php
+++ single.php
@@ -300,21 +300,6 @@ $(window).load(function() {
 				</div>
 				<div class="col-md-6 single-top-left simpleCart_shelfItem wow fadeInRight animated" data-wow-delay=".5s">
 					<h3><?php echo $rowproducto['nombre'] ?></h3>
-					<div class="single-rating">
-						<span class="starRating">
-							<input id="rating5" type="radio" name="rating" value="5" checked>
-							<label for="rating5">5</label>
-							<input id="rating4" type="radio" name="rating" value="4">
-							<label for="rating4">4</label>
-							<input id="rating3" type="radio" name="rating" value="3">
-							<label for="rating3">3</label>
-							<input id="rating2" type="radio" name="rating" value="2">
-							<label for="rating2">2</label>
-							<input id="rating1" type="radio" name="rating" value="1">
-							<label for="rating1">1</label>
-						</span>
-						
-					</div>
 					<h6 class="item_price"><?php echo $rowproducto['precio'] ?></h6>			
 					<p><?php  echo $rowproducto['descripcion'] ?></p>
 					
