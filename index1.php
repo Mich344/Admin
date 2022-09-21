b5d8462da84fef811fef1a3033cba9fcfcb7591e
Mich344 <104693039+Mich344@users.noreply.github.com>
2022-09-21 22:18:28 +0100

    Update index1.php

--- index1.php
+++ index1.php
@@ -63,7 +63,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
   <?
   session_start();
   session_regenerate_id(true);
-  
+
   if (isset($_REQUEST['accion']) && $_REQUEST['accion'] == "cerrar") {
     session_destroy();
     header("location: index1.php");
@@ -265,8 +265,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
             </ul>
             <div class="clearfix"> </div>
             <!--//navbar-collapse-->
-           
-          <!--//navbar-header-->
+
+            <!--//navbar-header-->
         </nav>
         <div id="cd-search" class="cd-search">
           <form>
@@ -278,7 +278,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
   </div>
   <!--//header-->
   <!--banner-->
-   
+
   <div class="banner">
     <div class="container">
       <div class="banner-text">
@@ -309,28 +309,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
               });
             </script>
           </div>
-
         </div>
-        
-        <!-- <div class="col-sm-7 banner-right wow fadeInRight animated" data-wow-delay=".5s">			
-					 <section class="slider grid">
-						<div class="flexslider">
-							<ul class="slides">
-								<li>
-									<h4>-30%</h4>
-									<img src="images/b1.png" alt="">
-								</li>
-								<li>
-									<h4>-25%</h4>
-									<img src="images/b2.png" alt="">
-								</li>
-								<li>
-									<h4>-32%</h4>
-									<img src="images/b3.png" alt="">
-								</li>
-							</ul>
-						</div>
-					</section> -->
         <!--FlexSlider-->
         <script defer src="js/jquery.flexslider.js"></script>
         <script type="text/javascript">
@@ -349,78 +328,11 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     </div>
   </div>
   <!--//banner-->
-  
-  <!--new-->
-  <div class="new">
-    <div class="container">
-      <div class="title-info wow fadeInUp animated" data-wow-delay=".5s">
-        <h3 class="title">Productos <span> Nuevos</span></h3>
-        <p>Productos subidos recientemente </p>
-      </div>
-      <div class="new-info">
-        <!-- INCLUIMOS LA BASE DE DATOS AL INDEX -->
-        <?php include_once "Basedata.php";
-        /*CONECTA LA BASE DE DATOS (BASEDATA) CON EL LOCALHOST, USUARIO, CONTRASEÑA Y DB*/
-        $con = mysqli_connect($host, $user, $pasword, $db);
-        /* UTILIZA UN REQUERIMIENTO DONDE SE VA A SELECCIONAR TODOS LOS PRODUCTOS CON SU ID, NOMBRE, PRECIO Y CANTIDAD DE LA TABLA PRODUCTOS  */
-        $query = "SELECT Id, nombre, precio, cantidad, talla, descripcion, imagen FROM productos";
-        /* DA UNA RESPUESTA CON EL MYSQL CON LOS REQUERIMIENTOS Y LA CONEXION */
-        $res = mysqli_query($con, $query);
-        //PRUEBA CON FOR 
-        //$row = mysqli_fetch_assoc($res);
-        /*for ($row=1;$row <4 ; $row++) { 
-						?>
-						<div class="col-md-3 gallery-grid gallery-grid2 wow flipInY animated" data-wow-delay=".9s">
-							<a href="single.php" title="Informacion del producto"><img src="images/g3.png" class="img-responsive" alt=""/></a>
-							<div class="gallery-text simpleCart_shelfItem">
-							<h5><a class="name" href="single.php"> no </a></h5>
-									<p><span class="item_price"><?php echo $row['Precio'] ?></span></p>
-								<h4 class="sizes">Sizes: <a href="#"> s</a> - <a href="#">m</a> - <a href="#">l</a> - <a href="#">xl</a> </h4>
-								<ul>
-									<li><a href="single.php" title="Informacion del producto"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span></a></li>
-									<li><a class="item_add" title="Añadir al carrito" href="#"><span class="glyphicon glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li>
-									
-								</ul>
-							</div>
-						</div>
-						<?php
-					}
-				?>
-						<div class="col-md-3 gallery-grid wow flipInY animated" data-wow-delay=".5s">
-							<a href="single.php" title="Informacion del producto"><img src="images/g3.png" class="img-responsive" alt=""/></a>
-							<div class="gallery-text simpleCart_shelfItem">
-							<h5><a class="name" href="single.php">si </a></h5>
-									<p><span class="item_price"><?php echo $row['Precio'] ?></span></p>
-								<h4 class="sizes">Sizes: <a href="#"> s</a> - <a href="#">m</a> - <a href="#">l</a> - <a href="#">xl</a> </h4>
-								<ul>
-									<li><a href="single.php" title="Informacion del producto"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span></a></li>
-									<li><a class="item_add" title="Añadir al carrito" href="#"><span class="glyphicon glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li>
-									
-								</ul>
-							</div>
-						</div>*/
-        while ($row = mysqli_fetch_assoc($res)) {
-        ?>
-          <div class="col-md-3 gallery-grid gallery-grid2 wow flipInY animated" data-wow-delay=".9s">
-            <a href="single.php?modulo=single&Id=<?php echo $row['Id'] ?>"><?php echo "<img src='upload/" . $row['imagen'] . "' class='img-responsive' alt=''>" ?></a>
-            <div class="gallery-text simpleCart_shelfItem">
-              <h5><a class="name" href="single.php"> <?php echo $row['nombre'] ?> </a></h5>
-              <p><span class="item_price"><?php echo $row['precio'] ?></span></p>
-              <h4 class="sizes">Sizes: <a href="#"> s</a> - <a href="#">m</a> - <a href="#">l</a> - <a href="#">xl</a> </h4>
-              <ul>
-                <li><a href="products.php"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span></a></li>
-                <li><a class="item_add" href=""><span class="glyphicon glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li>
-              </ul>
-            </div>
-          </div>
-        <?php
-        }
-        ?>
 
-        <div class="clearfix"> </div>
-      </div>
-    </div>
-  </div>
+  <!--new-->
+  <?php
+  include_once "Mproductos.php"
+  ?>
   <!--//new-->
   <!--trend-->
   <div class="trend wow zoomIn animated" data-wow-delay=".5s">
