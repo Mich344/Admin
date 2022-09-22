88b5f6159e4fd09cd62002d39dd9d5045a402681
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-09-21 22:30:45 -0500

    Update Productos.php

--- Productos.php
+++ Productos.php
@@ -122,20 +122,20 @@ else {
                       <td><?php echo $row['talla'] ?></td>
                       <td><?php echo $row['descripcion'] ?></td>
                       <td> <center><?php echo "<img width='80' height='80' src='/Admin/upload/".$row['imagen']."'>"?></center> </td>
-                      <?php 
+                     <center> <?php 
                          if (isset($_REQUEST['guardar'])){
                           include_once "Basedata.php";
                           // Llamar la base de datos desde el include_once.
                           $con = mysqli_connect($host, $user, $pasword, $db);
                          }
                         if ($row['estado'] == 1) {
-                              ?> <center> <?php echo '<td><button class="btn btn-success btn-xs">Activado</button></td>'; ?>  <center> <?php
+                              echo '<td><button class="btn btn-success btn-xs">Activado</button></td>';
                               }
                               else { 
-                              ?> <center>  echo '<td><button class="btn btn-danger btn-xs">Desactivado</button></td>'; ?>  <center> <?php
+                              echo '<td><button class="btn btn-danger btn-xs">Desactivado</button></td>'; 
                               }
                               ?>
-                      <td>
+                       <td> </center>
                         <a href="Panel.php?modulo=EditarP&Id= <?php echo $row['Id'] ?> " style="margin: 8px "> <i class="fas fa-edit"></i></a>
                         <a href="Panel.php?modulo=Productos&IdBorrar= <?php echo $row['Id'] ?> " class="text-danger borrar"> <i class="fas fa-trash"></i></a>
                         <a href="Panel.php?modulo=Productos&IdEstado1= <?php echo $row['Id'] ?> " class="btn btn-danger"></a> 
