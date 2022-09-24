3350ee6b8374a18da316c2229785cad97c221096
BelongMolded <105333595+BelongMolded@users.noreply.github.com>
2022-09-24 11:27:32 -0500

    Add files via upload

---
+++ carrucel.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!--trend-->
+<div class="trend wow zoomIn animated" data-wow-delay=".5s">
+    <div class="container">
+      <div class="trend-info">
+        <section class="slider grid">
+          <div class="flexslider trend-slider">
+            <ul class="slides">
+              <li>
+                <div class="col-md-5 trend-left">
+                  <img src="images/t1.png" alt="" />
+                </div>
+                <div class="col-md-7 trend-right">
+                  <h4>DESCUENTOS EN ACCESORIOS DEPORTIVOS</h4>
+                  <h5> 20% DE DESCUENTO</h5>
+                  <p>Compra online de cascos, guantes e hidratación deportiva a precios bajos con envío a todo Colombia, Somos la mejor opción a la hora de comprar!</p>
+                </div>
+                <div class="clearfix"></div>
+              </li>
+              <li>
+                <div class="col-md-5 trend-left">
+                  <img src="images/2t.png" alt="" />
+                </div>
+                <div class="col-md-7 trend-right">
+                  <h4>DESCUENTOS EN CAMISAS DEPORTIVAS <span>¡PARA TI!</span></h4>
+                  <h5>20% DE DESCUENTO</h5>
+                  <p>Encuentra el estilo en camisetas deportivas que estás buscando y en las mejores marcas como ADIDAS, NIKE , PUMA , DIESEL y muchas más.</p>
+                </div>
+                <div class="clearfix"></div>
+              </li>
+              <li>
+                <div class="col-md-5 trend-left">
+                  <img src="images/t3.png" alt="" />
+                </div>
+                <div class="col-md-7 trend-right">
+                  <h4>DESCUENTOS EN CONJUNTOS FEMENINOS</h4>
+                  <h5>20% DE DESCUENTO</h5>
+                  <p>Compra online Conjuntos deportivos mujer con pantalones deportivos, crop top y más. Ahorra tiempo y dinero haciendo tus compras en Tienda Moderna.</p>
+                </div>
+                <div class="clearfix"></div>
+              </li>
+              <li>
+                <div class="col-md-5 trend-left">
+                  <img src="images/t4.png" alt="" />
+                </div>
+                <div class="col-md-7 trend-right">
+                  <h4>DESCUENTOS EN ZAPATOS DEPORTIVOS <span>¡PARA TI!</span></h4>
+                  <h5>20% DE DESCUENTO </h5>
+                  <p>¡Zapatillas deportivas tanto de hombre como de mujer con un descuento del 20%! Modelos de marcas como Adidas y Nike con unas rebajas inimaginables que harán que tengas el estilo que mereces. Comodidad y calidad a un precio más bajo imposible.</p>
+                </div>
+                <div class="clearfix"></div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+  <!--//trend-->
+    
+</body>
+</html>
