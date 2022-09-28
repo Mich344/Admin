596321402138a07fe9d2d0e22a22d4368860e7d1
nicollearcos <107573307+nicollearcos@users.noreply.github.com>
2022-09-28 16:31:54 -0500

    Update about.php

--- about.php
+++ about.php
@@ -123,7 +123,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				<p>la mejor ropa es la de<strong> SPORTSWEARLINE</strong> </p>
 			</div>
 			<div class="about-info">
-				<p>La empresa <strong>SPORTSWEARLINE </strong> nacio con la intension de llegar a todo tipo de personas especialmente para insentivarlas a hacer deporte con productos de buena calidad y comoda para hacer todo tipo de ejercicio. ofrecemos una gran variedad de productos para los deportistas y personas interesadas en ejercitarse.</p>
+				<p>La empresa <strong>SPORTSWEARLINE </strong> nacio con la intension de llegar a todo tipo de personas especialmente para insentivarlas a hacer deporte con productos de buena calidad y comoda para hacer todo tipo de ejercicio. ofrecemos una gran variedad de productos para los deportistas y personas interesadas en ejercitarse. Avanzada, innovadora y siempre a la moda las colecciones deportivas, informales y benéficas de la marca inspiran a millones a disfrutar de una amplia gama de actividades con estilo y comodidad.</p>
 				<h4>Mas Acerca De Nosotros </h4>
 				<p> Nos enfocamos en ofrecer productos una muy buena calidad, funcionales y con diseños que se adapten al estilo de cada persona. Nuestros diseños estan alineados con la innovacion y las tendecias en el momento para que las personas se sientan con estilo y unicos. Fue creada en el 2022 con la intención de tener varios tipos de productos de diferentes marcas siendo nosotros distribuidores autorizados por cada marca que se utilice para ser vendida dentro de <strong>SPORTSWEARLINE. </strong></p>
 			</div>
