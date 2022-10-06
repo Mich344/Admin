5b3bf93f18b38a24a58ea354892df2e4179e4565
andres7w7 <109189149+andres7w7@users.noreply.github.com>
2022-10-05 20:49:51 -0500

    Update Usuarios.php

--- Administrador/Usuarios.php
+++ Administrador/Usuarios.php
@@ -9,86 +9,144 @@ if ($res){
   ?>
 
 <div class="alert alert-warning float-right" role="alert">
-     Usuario borrado con exito.
-    </div>
+     <?php
+     echo '<meta http-equiv= "refresh" content="0; url=Panel.php?modulo=Usuarios&mensaje=El Usuario ha sido borrado con exito" />';
+    ?>
+     </div>
 <?php
   }
 else {
+     echo '<meta http-equiv= "refresh" content="0; url=Panel.php?modulo=Usuarios&mensaje=No se pudo borrar el Usuario ' .  mysqli_error ($con) . ' " />';
+  }
+}
 ?>
-    <div class="alert alert-danger float-right" role="alert">
-      Error no se pudo borrar el usuario <?php echo mysqli_error ($con); ?> 
-    </div>
+<!-- ACTUALIZAR  -->
+<?php
+
+include_once "Basedata.php";
+$con = mysqli_connect($host, $user, $pasword, $db);
+if(isset($_REQUEST['IdEstado1'])){
+$Id = mysqli_real_escape_string($con, $_REQUEST['IdEstado1']??''); 
+$query = "UPDATE usuario SET estado = '1' WHERE Id = '".$Id."';";
+$res = mysqli_query($con, $query);
+if ($res){
+echo '<meta http-equiv= "refresh" content="0; url=Panel.php?modulo=Usuarios&mensaje=El Usuarios ha sido cambiado de estado con exito" />';
+  }
+else {
+?>
+     <?php
+     echo '<meta http-equiv= "refresh" content="0; url=Panel.php?modulo=Usuarios&mensaje=No se pudo cambiar de estado del Usuarios ' .  mysqli_error ($con) . ' " />';
+    ?>
     <?php
   }
 }
 ?>
+<?php
+include_once "Basedata.php";
+$con = mysqli_connect($host, $user, $pasword, $db);
 
-<div class="content-wrapper">
+if(isset($_REQUEST['IdEstado2'])){
+$Id = mysqli_real_escape_string($con, $_REQUEST['IdEstado2']??''); 
+$query = "UPDATE usuarios SET estado = '0' WHERE Id = '".$Id."';";
+$res = mysqli_query($con, $query);
+if ($res){
+  echo '<meta http-equiv= "refresh" content="0; url=Panel.php?modulo=Usuarios&mensaje=El Usuarios ha sido cambiado de estado con exito" />';
+  }
+else {
+echo '<meta http-equiv= "refresh" content="0; url=Panel.php?modulo=Usuarios&mensaje=No se pudo cambiar de estado del Usuarios ' .  mysqli_error ($con) . ' " />';
+  }
+}
+?>
+<div class="content-wrapper" style="background-color: black;">
   <!-- Content Header (Page header) -->
   <section class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1><strong>Usuarios</strong></h1>
+          <h1><strong style="color:white;">Usuarios</strong></h1>
+          <div>
+            <hr>
+            <a href="Panel.php?modulo=CrearU" type="button" class="btn btn-success">Crear Usuarios</a>
+          </div> 
         </div>
-
       </div>
     </div><!-- /.container-fluid -->
   </section>
-
   <!-- Main content -->
   <section class="content">
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
           <div class="card">
-
             <!-- /.card-header -->
             <div class="card-body">
-              <table id="example2" class="table table-bordered table-hover">
+             <!--  <?php //var_dump($_SERVER) ?> -->
+              <table  class="table table-bordered table-hover">
                 <thead>
                   <tr>
-                    <th>Id</th>
-                    <th>Email</th>                  
+                    <th>Id</th>                
                     <th>Nombre</th>
-                     <th>Apellido</th>
-                    <th>Cuiudad</th>
+                    <th>Apellido</th>
+                    <th>Email</th>
+                    <th>Ciudad</th>
+                    <th>Departamento</th>
                     <th>Direccion</th>
                     <th>Telefono</th>
-                    <th>Tip documento</th>
-                    <th>Num documneto</th>
-                    <th>Fech nacimiento</th>
-                   
-                    <th>
-                     Acciones                          
-                      <a href="Panel.php?modulo=CrearU"> <i class="fa fa-user-plus" aria-hidden="true"></i></i></a>
-                    </th>
+                    <th>Tipo Documento</th>
+                    <th>Numero Documento</th>
+                    <th>Fecha Nacimiento</th>
+                    <th>Estado</th>
+                    <th>Funciones <font size=2></font></th>
                   </tr>
                 </thead>
-
                 <tbody>
                 <?php
                   include_once "Basedata.php";
-                   $con = mysqli_connect($host, $user, $pasword, $db);
-                    $query = "SELECT Id, email, nombre, direccion, apellido, ciudad, telefono, tip_doc, num_doc, fech_nac from usuario;";
+                    $con = mysqli_connect($host, $user, $pasword, $db);
+                    $query = "SELECT Id , nombre, apellido, email, ciudad, departamento, direccion, telefono, tip_doc, num_doc, fech_nac, estado  FROM usuario;";
                     $res = mysqli_query($con, $query);
+                    
                     while ($row = mysqli_fetch_assoc($res)){
-                   
                   ?>
                   <tr>
-                     <td><?php echo $row['Id']?></td>
-                     <td><?php echo $row['email'] ?></td>
+                      <td><?php echo $row['Id']?></td>
                       <td><?php echo $row['nombre'] ?></td> 
-                      <td><?php echo $row['direccion'] ?></td> 
-                      <td><?php echo $row['apellido'] ?></td>
+                      <td><?php echo $row['apellido'] ?></td> 
+                      <td><?php echo $row['email'] ?></td>
                       <td><?php echo $row['ciudad'] ?></td>
+                      <td><?php echo $row['departamento'] ?></td>
+                      <td><?php echo $row['direccion'] ?></td>
                       <td><?php echo $row['telefono'] ?></td>
                       <td><?php echo $row['tip_doc'] ?></td>
                       <td><?php echo $row['num_doc'] ?></td>
                       <td><?php echo $row['fech_nac'] ?></td>
-                      <td>
-                           <a href="Panel.php?modulo=EditarU&Id= <?php echo $row['Id'] ?> " style="margin: 8px "> <i class="fas fa-edit"></i></a>
-                        <a href="Panel.php?modulo=Usuarios&IdBorrar= <?php echo $row['Id'] ?> " class="text-danger borrar"> <i class="fas fa-trash"></i></a>
+                     <?php 
+                         if (isset($_REQUEST['guardar'])){
+                          include_once "Basedata.php";
+                          // Llamar la base de datos desde el include_once.
+                          $con = mysqli_connect($host, $user, $pasword, $db);
+                         }
+                        if ($row['estado'] == 1) {
+                          ?>
+                              <center><td><button class="btn btn-success btn-xs">Activo</button></td></center>
+
+                              <?php
+                              }
+
+                              else { 
+                                ?>
+                                  <center> <td><button class="btn btn-danger btn-xs">Inactivo</button></td> </center>
+                              <?php
+                              }
+                              ?>
+                       <td>
+                          <a href="Panel.php?modulo=EditarU&Id= <?php echo $row['Id'] ?> " style="margin: 8px "><i class="fas fa-user-edit" title="Editar Usuario"></i></a>
+
+                          <a href="Panel.php?modulo=Usuarios&IdEstado1= <?php echo $row['Id'] ?> " class="btn btn-md" style="color:green;"> <i class="fas fa-user-plus" aria-hidden="true" title="Activo"></i></a>
+
+                          <a href="Panel.php?modulo=Usuarios&IdEstado2= <?php echo $row['Id'] ?> " class="btn btn-md" style="color:red;"><i class="fas fa-user-minus" aria-hidden="true" title="Inactivo"></i></a>
+
+                          <a href="Panel.php?modulo=Usuarios&IdBorrar= <?php echo $row['Id'] ?> " class="text-danger borrar"><i class="fas fa-trash-alt" title="Eliminar"></i></a>
                       </td>
                     </tr>
                   <?php
