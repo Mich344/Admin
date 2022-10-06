ed087671515349fdebb4f33a59d5ce054808093a
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-10-06 00:51:22 -0500

    Update CrearP.php

--- Administrador/CrearP.php
+++ Administrador/CrearP.php
@@ -75,7 +75,7 @@ if (isset($_REQUEST['guardar'])) {
               <form action="Panel.php?modulo=CrearP" method="post" enctype="multipart/form-data">
                 <div class="for-group">
                   <label>Nombre</label>
-                  <input type="text" name="nombre" class="form-control" required="" pattern="[a-zA-Z]+" minlength="2">
+                  <input type="text" name="nombre" class="form-control" required="" pattern="[a-zA-Z]+" minlength="3">
                 </div>
                 <div class="for-group">
                   <label>Precio</label>
