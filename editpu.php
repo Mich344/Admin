65570b155085a5f8e938482b26e45dcefe75e033
andres7w7 <109189149+andres7w7@users.noreply.github.com>
2022-10-11 20:58:51 -0500

    Update editpu.php

--- editpu.php
+++ editpu.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,66 +8,110 @@
     <title>perfil</title>
     <link rel="stylesheet" href="css/boostrap.min.css">
     <script src="js/jquery.js"> </script>
-    <script src="js/bootstrap.min.js"> </script> 
+    <script src="js/bootstrap.min.js"> </script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
+
+<?php
+include_once "Administrador/Basedata.php";
+$con = mysqli_connect($host, $user, $pasword, $db);
+if (isset($_REQUEST['guardar'])) {
+  $Id = mysqli_real_escape_string($con, $_REQUEST['Id'] ?? '');
+  $nombre = mysqli_real_escape_string($con, $_REQUEST['nombre'] ?? '');
+  $apellido = mysqli_real_escape_string($con, $_REQUEST['apellido'] ?? '');
+  $email = mysqli_real_escape_string($con, $_REQUEST['email'] ?? '');
+  $pasword = mysqli_real_escape_string($con, $_REQUEST['pasword'] ?? '');
+  $ciudad = mysqli_real_escape_string($con, $_REQUEST['ciudad'] ?? '');
+  $depar = mysqli_real_escape_string($con, $_REQUEST['depar'] ?? '');
+  $direccion = mysqli_real_escape_string($con, $_REQUEST['direccion'] ?? '');
+  $telefono = mysqli_real_escape_string($con, $_REQUEST['telefono'] ?? '');
+  $tip_doc = mysqli_real_escape_string($con, $_REQUEST['tip_doc'] ?? '');
+  $num_doc = mysqli_real_escape_string($con, $_REQUEST['num_doc'] ?? '');
+  $fech_nac = mysqli_real_escape_string($con, $_REQUEST['fech_nac'] ?? '');
+
+  $query = "UPDATE usuario SET nombre = '" . $nombre . "' ,apellido = '" . $apellido . "' ,email = '" . $email . "' ,pasword = '" . $pasword . "' ,ciudad = '" . $ciudad . "' ,departamento = '" . $depar . "' ,direccion = '" . $direccion . "' ,telefono = '" . $telefono . "' ,tip_doc = '" . $tip_doc . "' ,num_doc = '" . $num_doc . "' ,fech_nac = '" . $fech_nac . "' WHERE Id = '" . $Id . "';";
+  //Restultados 
+  $res = mysqli_query($con, $query);
+  if ($res) {
+    echo '<meta http-equiv= "refresh" content="0; url=Panel.php?modulo=Usuarios&mensaje= ' . $nombre . ' Editado correctamente" />';
+  } else {
+?>
+    <div class="alert alert-danger" role="alert">
+      Error al editar producto <?php echo mysqli_error($con) ?>
+    </div>
+<?php
+  }
+}
+// Leer los usuarios 
+// ((mysqli_real_escape_string)) Significado llama consultas preparadas 
+$Id = mysqli_real_escape_string($con, $_REQUEST['Id'] ?? '');
+// Seleccionar los datos //
+$query = "SELECT Id, nombre, apellido, email, pasword, ciudad, departamento, direccion, telefono, tip_doc, num_doc, fech_nac from usuario WHERE  Id = '" . $Id . "';";
+// Pasar la conexion $con, $query y almacenar en la variable $res. //
+$res = mysqli_query($con, $query);
+// (mysqli_fetch_assoc) Entregar un registro con el almacenamiento de la variable $res
+$row = mysqli_fetch_assoc($res);
+
+?>
 <body>
 
 
-<script>
-// Some random colors
-const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+    <script>
+        // Some random colors
+        const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 
-const numBalls = 50;
-const balls = [];
+        const numBalls = 50;
+        const balls = [];
 
-for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
-  ball.classList.add("ball");
-  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-  ball.style.transform = `scale(${Math.random()})`;
-  ball.style.width = `${Math.random()}em`;
-  ball.style.height = ball.style.width;
-  
-  balls.push(ball);
-  document.body.append(ball);
-}
+        for (let i = 0; i < numBalls; i++) {
+            let ball = document.createElement("div");
+            ball.classList.add("ball");
+            ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+            ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+            ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+            ball.style.transform = `scale(${Math.random()})`;
+            ball.style.width = `${Math.random()}em`;
+            ball.style.height = ball.style.width;
 
-// Keyframes
-balls.forEach((el, i, ra) => {
-  let to = {
-    x: Math.random() * (i % 2 === 0 ? -11 : 11),
-    y: Math.random() * 12
-  };
+            balls.push(ball);
+            document.body.append(ball);
+        }
 
-  let anim = el.animate(
-    [
-      { transform: "translate(0, 0)" },
-      { transform: `translate(${to.x}rem, ${to.y}rem)` }
-    ],
-    {
-      duration: (Math.random() + 1) * 2000, // random duration
-      direction: "alternate",
-      fill: "both",
-      iterations: Infinity,
-      easing: "ease-in-out"
-    }
-  );
-});
-</script>
-<!-- SEPARACION DE JAVASCRIPT Y DISEÑO -->
-<style>
-.ball {
-  position: absolute;
-  border-radius: 100%;
-  opacity: 0.7;
-}
-</style>
+        // Keyframes
+        balls.forEach((el, i, ra) => {
+            let to = {
+                x: Math.random() * (i % 2 === 0 ? -11 : 11),
+                y: Math.random() * 12
+            };
+
+            let anim = el.animate(
+                [{
+                        transform: "translate(0, 0)"
+                    },
+                    {
+                        transform: `translate(${to.x}rem, ${to.y}rem)`
+                    }
+                ], {
+                    duration: (Math.random() + 1) * 2000, // random duration
+                    direction: "alternate",
+                    fill: "both",
+                    iterations: Infinity,
+                    easing: "ease-in-out"
+                }
+            );
+        });
+    </script>
+    <!-- SEPARACION DE JAVASCRIPT Y DISEÑO -->
+    <style>
+        .ball {
+            position: absolute;
+            border-radius: 100%;
+            opacity: 0.7;
+        }
+    </style>
 
 
 
@@ -76,16 +121,13 @@ balls.forEach((el, i, ra) => {
             <div class="col-md-8 offset-md-2">
                 <ul class="nav nav-tabs" id="myTab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="perfil-tab" data-toggle="tab" href="#perfil" role="tab"
-                            aria-controls="home" aria-selected="true">Datos Usuario</a>
+                        <a class="nav-link active" id="perfil-tab" data-toggle="tab" href="#perfil" role="tab" aria-controls="home" aria-selected="true">Datos Usuario</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="contraseña-tab" data-toggle="tab" href="#cambiocontra" role="tab"
-                            aria-controls="profile" aria-selected="false">Cambiar contraseña</a>
+                        <a class="nav-link" id="contraseña-tab" data-toggle="tab" href="#cambiocontra" role="tab" aria-controls="profile" aria-selected="false">Cambiar contraseña</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="envio-tab" data-toggle="tab" href="#datosenvio" role="tab"
-                            aria-controls="profile" aria-selected="false">Datos de envio</a>
+                        <a class="nav-link" id="envio-tab" data-toggle="tab" href="#datosenvio" role="tab" aria-controls="profile" aria-selected="false">Datos de envio</a>
                     </li>
                 </ul>
                 <div class="tab-content" id="myTabContent">
@@ -93,9 +135,7 @@ balls.forEach((el, i, ra) => {
                     <div class="tab-pane fade show active" id="perfil" role="tabpanel" aria-labelledby="perfil-tab">
                         <div class="row">
                             <div class="col-4">
-                                <img class="img-thumbnail"
-                                    src="https://cdn2.iconfinder.com/data/icons/website-icons/512/User_Avatar-512.png"
-                                    alt="">
+                                <img class="img-thumbnail" src="https://cdn2.iconfinder.com/data/icons/website-icons/512/User_Avatar-512.png" alt="">
                             </div>
                             <div class="col-8">
                                 <div class="form-group row">
@@ -104,7 +144,7 @@ balls.forEach((el, i, ra) => {
                                         <input type="text" class="form-control" value="">
                                     </div>
                                 </div>
-                                 <div class="form-group row">
+                                <div class="form-group row">
                                     <label for="tel" class="col-2">Apellidos</label>
                                     <div class="col-8">
                                         <input type="text" class="form-control" value="">
@@ -119,7 +159,7 @@ balls.forEach((el, i, ra) => {
                                 <div class="form-group text-center">
                                     <button class="btn btn-info">Actualizar</button>
                                     <button class="btn btn-danger">Cancelar</button>
-                                    
+
                                 </div>
                             </div>
                         </div>
@@ -164,7 +204,7 @@ balls.forEach((el, i, ra) => {
                             <div class="form-group row">
                                 <label for="ciudad" class="col-4">Ciudad</label>
                                 <div class="col-8">
-                                  <!-- SE SUPONE QUE AQUI DEBE DE HABER UNA LISTA DE LAS CIUDADES -->
+                                    <!-- SE SUPONE QUE AQUI DEBE DE HABER UNA LISTA DE LAS CIUDADES -->
                                     <input type="ciudad" class="form-control">
                                 </div>
                             </div>
@@ -174,7 +214,7 @@ balls.forEach((el, i, ra) => {
                                     <input type="tel" class="form-control">
                                 </div>
                             </div>
-                             <div class="form-group row">
+                            <div class="form-group row">
                                 <label for="contraseña" class="col-4">Barrio</label>
                                 <div class="col-8">
                                     <input type="text" class="form-control">
@@ -191,4 +231,5 @@ balls.forEach((el, i, ra) => {
         </div>
     </div>
 </body>
+
 </html>
