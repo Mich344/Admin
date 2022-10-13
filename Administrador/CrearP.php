c84f4392d2c4e77a2261a9a185bb7910f051513a
Valery Salazar <88462449+valerysalazar0812@users.noreply.github.com>
2022-10-12 19:53:13 -0500

    Update CrearP.php

--- Administrador/CrearP.php
+++ Administrador/CrearP.php
@@ -9,7 +9,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"> -->
   <!-- <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet"> -->
   <!-- ACOMODA EL SELECT MULTIPLE -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.8.1/css/bootstrap-select.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.8.1/js/bootstrap-select.js"></script>
@@ -132,11 +132,10 @@ if (isset($_REQUEST['guardar'])) {
                     ?>
                   </select>
                 </div>
-                
-                <label>Talla(s)</label>
+                    <br>
                 <div class="for-group" >
-                  <select class="selectpicker" multiple="" name="talla[]" > 
-                    <option value="" disabled selected>Elegir tallas</option>
+                  <label for="for-group">Talla(s)</label>
+                  <select class="selectpicker" multiple="" name="talla[]" required> 
                     <option value="No aplica">No aplica</option>  
                     <option value="Talla Unica">Talla Unica</option>
                     <optgroup label="Camisas">
@@ -176,7 +175,7 @@ if (isset($_REQUEST['guardar'])) {
                     </optgroup>
                   </select>
                 </div>
-
+                    <br>
                 <div class="photo" style="width:450px">
                   <label>Imagen</label>
                   <input id="imagen" class="form-control" type="file" name="imagen" multiple="multiple" required="" accept="image/png, image/jpg, image/jpeg, image/pjpeg">
